4fb2e893afd7b3b581c16e7aa3076caf803b0d34
Lucas Gragera <lucasgragera1@gmail.com>
2023-12-27 18:25:06 -0300

    Estrategia de autenticación por terceros + JWT

--- src/daos/mongodb/user.dao.js
+++ src/daos/mongodb/user.dao.js
@@ -1,6 +1,67 @@
+//import  {createHash, isValidPassword}  from  '../utils';
 import { UserModel } from './models/user.model.js';
 
 export default class UserDao {
+    async registerUser(user) {
+        // console.log('userDao:::', user);
+        try {
+          const { email, password } = user;
+          const existUser = await UserModel.findOne({ email });
+          if (!existUser) {
+            if (email === "adminCoder@coder.com" && password === "adminCod3r123") {
+              return await UserModel.create({
+                ...user,
+                password: createHash(password),
+                role: "admin",
+              });
+            }
+            return await UserModel.create({
+              ...user,
+              password: createHash(password),
+            });
+          } else return false;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
+      async loginUser(user) {
+        try {
+          const { email, password } = user;
+          const userExist = await UserModel.findOne({ email });
+          if (userExist) {
+            const isValid = isValidPassword(password, userExist);
+            console.log('isValid__', isValid);
+            if (!isValid) return false;
+            else return userExist;
+            // !isValid ? false : userExist
+          } return false;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      async createUser(user) {
+        try {
+          const { email, password } = user;
+          const existUser = await UserModel.findOne({email});
+          if(!existUser){
+            if(email === 'adminCoder@coder.com' && password === 'adminCod3r123'){
+              return await UserModel.create({
+                ...user,
+                password: createHash(password),
+                role: 'admin'
+            });
+            } 
+              return await UserModel.create({
+                  ...user,
+                  password: createHash(password),
+              });
+            } else return false;
+          } catch (error) {
+            console.log(error)
+            throw new Error(error)
+          }
+        }
 
     async getById(id) {
         try {
@@ -14,6 +75,8 @@ export default class UserDao {
             // throw new Error(error)
         }
     }
+   
+    
 
     async getByEmail(email) {
         try {
